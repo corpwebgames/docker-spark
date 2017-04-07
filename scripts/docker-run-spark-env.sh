ef9fa5f2959cbcab8ab1d54f261c50cffea566e1
ebirukov <john_soft@mail.ru>
2017-04-07 12:25:46 +0300

    Update docker-run-spark-env.sh
    add all udf libs

--- scripts/docker-run-spark-env.sh
+++ scripts/docker-run-spark-env.sh
@@ -18,8 +18,7 @@ export SPARK_LOCAL_HOSTNAME=$DOCKER_HOSTNAME
 #cat /usr/local/spark/conf/template-core-site.xml | m4 -DAWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID -DAWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY > /usr/local/spark/conf/core-site.xml
 aws s3 cp s3://$S3_BUCKET_CONF/hive/config/core-site.xml /usr/local/spark/conf/core-site.xml > /dev/null
 aws s3 cp s3://$S3_BUCKET_CONF/hive/config/hive-site.xml /usr/local/spark/conf/hive-site.xml > /dev/null
-aws s3 cp s3://$S3_BUCKET_CONF/hive/config/udf/hive-geoip.jar /usr/local/spark/jars/ > /dev/null
-aws s3 cp s3://$S3_BUCKET_CONF/hive/config/udf/geoip-api.jar /usr/local/spark/jars/ > /dev/null
+aws s3 cp --recursive --exclude "*" --include "*.jar" s3://$S3_BUCKET_CONF/hive/config/udf/ /usr/local/spark/jars/ > /dev/null
 env | grep SPARK | awk '{print "export \"" $0 "\""}' > /usr/local/spark/conf/spark-env.sh
 export CLASSPATH="/usr/local/spark/jars/mysql-connector-java.jar"
 export PATH="$PATH:/usr/local/spark/bin"
