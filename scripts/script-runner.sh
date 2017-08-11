32b2b7fafa46ddf18ee4d51eccb618383bad71fe
v.shago <dpatriot@gmail.com>
2017-08-11 16:17:37 +0300

    save logs

--- scripts/script-runner.sh
+++ scripts/script-runner.sh
@@ -18,9 +18,16 @@ docker-run-spark-env.sh $SPARK_HOME/bin/spark-submit \
 --driver-memory $DRIVER_MEMORY \
 --driver-class-path $SPARK_DRIVER_CLASSPATH \
 --jars $SPARK_JARS \
-$file $ARGS 2>&1
+$file $ARGS 2>&1 | tee $file-$ID.out
+
+_status=${PIPESTATUS[0]}
+_output=`cat $file-$ID.out | head -c65535`
+
+times=$(date +"%Y%m%d%H%M")
+  if [ "$S3_LOG_BUCKET" ]; then aws s3 cp $file-$ID.out s3://$S3_LOG_BUCKET/docker/$CATEGORY/$ID/$file-$times.out; fi
+echo "status $_status"
+  exit $_status
 
 else
   send "File $1 not found" "255"
 fi
-
